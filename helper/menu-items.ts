92594e1deffb921a4615628640b8fbf76109b338
exodos <exodosbob@gmail.com>
2023-05-11 13:13:11 +0300

    dynamic icons

---
+++ helper/menu-items.ts
@@ -0,0 +1,16 @@
+"use client";
+import * as icons from "react-icons/all";
+
+export const menuItems = [
+  {
+    id: 1,
+    title: "Smart Identity",
+    subMenu: [
+      {
+        id: 11,
+        title: "ZKBio Time API",
+        icon: icons.BiTimer,
+      },
+    ],
+  },
+];
